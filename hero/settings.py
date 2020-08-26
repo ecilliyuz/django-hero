4ad93f91621bad8180966aa26861ab78b7be2818
ecilliyuz <37616237+ecilliyuz@users.noreply.github.com>
2020-08-26 17:38:31 +0300

    Update settings.py

--- hero/settings.py
+++ hero/settings.py
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+'
 ]
 
 ROOT_URLCONF = 'hero.urls'
@@ -123,3 +125,5 @@ STATIC_URL = '/static/'
 #	os.path.join(BASE_DIR, "static")
 #]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
